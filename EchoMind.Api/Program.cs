6590a6b64fef91bfda64ff087dc449bf6caf73c2
EletherioAlexandre <eletherioalexandre@gmail.com>
2025-04-23 23:23:45 -0300

    feat: add EchoMind .NET API structure with validation

---
+++ EchoMind.Api/Program.cs
@@ -0,0 +1,31 @@
+using EchoMind.Application.UseCases.Feedback.Register;
+using FluentValidation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddScoped<RegisterFeedbackUseCase>();
+
+builder.Services.AddValidatorsFromAssemblyContaining<RegisterFeedbackValidator>();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ EchoMind.Application/UseCases/Feedback/Register/RegisterFeedbackUseCase.cs
@@ -0,0 +1,34 @@
+﻿using EchoMind.Communication.Requests;
+using FluentValidation;
+
+namespace EchoMind.Application.UseCases.Feedback.Register
+{
+    public class RegisterFeedbackUseCase
+    {
+
+        private readonly IValidator<RequestRegisterFeedbackJson> _validator;
+
+        public RegisterFeedbackUseCase(IValidator<RequestRegisterFeedbackJson> validator)
+        {
+            _validator = validator;
+        }
+
+        public void Execute(RequestRegisterFeedbackJson request)
+        {
+            Validate(request);
+        }
+
+        public void Validate(RequestRegisterFeedbackJson request)
+        {
+
+            var result = _validator.Validate(request);
+
+            if (!result.IsValid)
+            {
+                List<string> errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+                throw new ValidationException(string.Join(" | ", errors));
+            }
+
+        }
+    }
+}
